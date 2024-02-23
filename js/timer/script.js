556ad0b8715f50dd64e27acf8886a95d5c072348
Vaeep <diogobonofre@gmail.com>
2024-02-23 03:37:20 -0300

    pomodoro timer basic structure

--- js/timer/script.js
+++ js/timer/script.js
@@ -1,31 +1,47 @@
-const n1 = document.getElementById("n1");
-const n2 = document.getElementById("n2");
-const n3 = document.getElementById("n3");
-const n4 = document.getElementById("n4");
+const timer = document.getElementById("timer");
 
-setInterval(function attTimer() {
-	let d = new Date();
-	let m = d.getMinutes().toString().split("");
-	let s = d.getSeconds().toString().split("");
+let interval;
 
-	switch (m[1]) {
-		case undefined:
-			n1.innerText = 0
-			n2.innerText = m[0];
-			break;
-		default:
-			n1.innerText = m[0];
-			n2.innerText = m[1];
-			break;
-	}
-	switch (s[1]) {
-		case undefined:
-			n3.innerText = 0
-			n4.innerText = s[0];
-			break;
-		default:
-			n3.innerText = s[0];
-			n4.innerText = s[1];
-			break;
-	}
-}, 1000);
+const defaultMinutes = 10;
+let defaultSeconds = defaultMinutes * 60;
+
+function convertTime() {
+	let hours = Math.floor(defaultSeconds / 60 / 60);
+	let minutes = Math.floor(defaultSeconds / 60);
+	let seconds = defaultSeconds % 60;
+
+	seconds = seconds < 10 ? '0' + seconds : seconds;
+	minutes = minutes < 10 ? '0' + minutes : minutes;
+	hours = hours < 10 ? '0' + hours : hours;
+
+	return `${hours}:${minutes}:${seconds}`;
+}
+
+function startTimer() {
+	updateCountdown();
+	interval = setInterval(updateCountdown, 1000);
+}
+
+function updateCountdown() {
+	timer.innerHTML = convertTime();
+	defaultSeconds--;
+}
+
+function resetTimer() {
+	clearInterval(interval);
+	defaultSeconds = defaultMinutes * 60;
+	timer.innerText = convertTime();
+}
+
+function stopTimer() {
+	clearInterval(interval);
+}
+
+function toggleMenu() {
+	const menu = document.getElementById("floating-config-menu");
+	const overlay = document.getElementById("overlay");
+	menu.classList.toggle("active");
+	overlay.classList.toggle("active");
+}
+
+resetTimer();
