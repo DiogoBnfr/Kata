6cd8aaadf0e62422e0e639f27f1e32248f929fbf
Vaeep <diogobonofre@gmail.com>
2024-02-29 17:51:07 -0300

    constructor methods and implementations tests in JS

--- tests/js/constructors_dry_methods.js
+++ tests/js/constructors_dry_methods.js
@@ -1,11 +1,12 @@
+function setTo(x = 0, y = 0) {
+	this.x = x;
+	this.y = y;
+}
+
 function Point(x = 0, y = 0) {
 	this.x = x;
 	this.y = y;
-
-	this.setTo = function(x = 0, y = 0) {
-		this.x = x;
-		this.y = y;
-	}
+	this.setTo = setTo;
 }
 
 var p1 = new Point(-2, 8);
@@ -18,5 +19,9 @@ p1.setTo();
 console.log(p1.x, p1.y);
 
 var p2 = new Point(1, 2);
-console.log(p1.setTo == p2.setTo);
+
 console.log(p1.setTo === p2.setTo);
+
+console.log(p1 instanceof Point);
+
+console.log(p1.constructor);
