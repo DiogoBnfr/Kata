e297500bf72c46ee21b288b031db85fb02329ec6
Vaeep <diogobonofre@gmail.com>
2024-03-13 00:30:49 -0300

    recursive digital root in C

---
+++ c/digital_root/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int digital_root(int n) {
+  int sum = 0;
+  while (n > 0) {
+    int digit = n % 10;
+    sum += digit;
+    n /= 10;
+  }
+  if (sum > 9)
+    return digital_root(sum);
+  else
+    return sum;
+}
+
+int main(void) {
+  int n = 16;
+  printf("(n : dr) -> (%d : %d)\n", n, digital_root(n));
+}
