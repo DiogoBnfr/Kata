70d8c3a7f03d78f6471eee69925d8e2533f51091
Vaeep <diogobonofre@gmail.com>
2024-03-21 14:02:35 -0300

    Some fixes in ex_1_13 display functions

--- c/The-C-Programming-Language/ex_1_13/word_lenght_histogram.c
+++ c/The-C-Programming-Language/ex_1_13/word_lenght_histogram.c
@@ -1,9 +1,10 @@
 #include <linux/limits.h>
 #include <stdio.h>
+#include <string.h>
 
 #define BUFFER 100
 
-int main(void) {
+int main(int argc, char *argv[]) {
   int histogram[BUFFER];
 
   for (int i = 0; i < BUFFER; ++i) {
@@ -21,41 +22,42 @@ int main(void) {
       ++word_lenght;
   }
 
-  int max_frequency = 0;
-  for (int i = 0; i < BUFFER; ++i) {
-    if (histogram[i] > max_frequency) {
-      max_frequency = histogram[i];
+  if (argc < 2) {
+    // Print vertical histogram
+    int max_frequency = 0;
+    for (int i = 0; i < BUFFER; ++i) {
+      if (histogram[i] > max_frequency) {
+        max_frequency = histogram[i];
+      }
     }
-  }
-
-  // print horizontal histogram
-  printf("LEN  WORDS\n");
-  for (int i = 0; i < BUFFER; ++i) {
-    if (histogram[i] != 0) {
-      printf("%3d  ", i);
-      for (int j = 0; j < histogram[i]; ++j)
-        printf("▆");
-      printf(" %d\n", histogram[i]);
+    for (int i = max_frequency; i > 0; --i) {
+      printf("\n%3d  ", i);
+      for (int j = 0; j < BUFFER; ++j) {
+        if (histogram[j] >= i)
+          printf("  █");
+      }
     }
-  }
-  printf("\n");
-
-  // Print vertical histogram
-  for (int i = max_frequency; i > 0; --i) {
-    printf("%3d  ", i);
-    for (int j = 0; j < BUFFER; ++j) {
-      if (histogram[j] >= i)
-        printf("  █");
+    printf("\n     ");
+    for (int i = 0; i < BUFFER; ++i) {
+      if (histogram[i] != 0) {
+        printf("%3d", i);
+      }
     }
     printf("\n");
-  }
-  printf("     ");
-  for (int i = 0; i < BUFFER; ++i) {
-    if (histogram[i] != 0) {
-      printf("%3d", i);
+  } else {
+    if (strcmp(argv[1], "-h") == 0) {
+      // print horizontal histogram
+      for (int i = 0; i < BUFFER; ++i) {
+        if (histogram[i] != 0) {
+          printf("\n%3d  ", i);
+          for (int j = 0; j < histogram[i]; ++j)
+            printf("▆");
+          printf(" %d", histogram[i]);
+        }
+      }
+      printf("\n");
     }
   }
-  printf("\n");
 
   return 0;
 }
